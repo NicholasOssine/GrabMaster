dc81c5d2d5058c701c5661033483bb9b4f24c5b6
NicholasOssine <ossinenicholas@gmail.com>
2026-07-30 12:13:44 -0400

    Load settings in the content script

---
+++ lobby.js
@@ -0,0 +1,14 @@
+const DEFAULTS = {
+  enabled: false,
+  categories: ["Bullet", "Blitz", "Rapid"],
+  gameType: "any",
+  minRating: 1400,
+  maxRating: 1800,
+  acceptUnrated: false,
+};
+
+let settings = { ...DEFAULTS };
+
+chrome.storage.sync.get(DEFAULTS, (storedSettings) => {
+  settings = storedSettings;
+});
