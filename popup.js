819077469c802d53a785df7abb24ddbd101ed2b4
NicholasOssine <ossinenicholas@gmail.com>
2026-07-30 12:13:42 -0400

    Save form settings on submit

--- popup.js
+++ popup.js
@@ -6,3 +6,28 @@ const DEFAULTS = {
   maxRating: 1800,
   acceptUnrated: false,
 };
+
+function onButton() {
+  const categories = [];
+  for (const checkbox of document.querySelectorAll("#categories input")) {
+    if (checkbox.checked) categories.push(checkbox.value);
+  }
+
+  let gameType = "any";
+  for (const radioButton of document.querySelectorAll(
+    'input[name="gameType"]',
+  )) {
+    if (radioButton.checked) gameType = radioButton.value;
+  }
+
+  chrome.storage.sync.set({
+    enabled: true,
+    categories: categories,
+    gameType: gameType,
+    minRating: Number(document.getElementById("minRating").value),
+    maxRating: Number(document.getElementById("maxRating").value),
+    acceptUnrated: document.getElementById("acceptUnrated").checked,
+  });
+}
+
+document.getElementById("go").addEventListener("click", onButton);
